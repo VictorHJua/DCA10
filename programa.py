db341a0eb8a295002d8a7d77c3b7258cf5d6368e
Victor Hernandez Justamante <vhj1@gcloud.ua.es>
2022-12-22 17:05:25 +0100

    v1.7

--- programa.py
+++ programa.py
@@ -1,14 +1,17 @@
+
+
+
 def elegirOperacion():
-    print("¿Que operación quiere realizar?\n0. Salir\n1. Suma\n2. Resta\n3. Multiplicación\n4. División")
+    print("¿Que operación quiere realizar?\n0. Salir\n1. Suma\n2. Resta\n3. Multiplicación\n4. División\n5. Libre")
     operacion = ""
     opcion = -1
-    while opcion < 0 or opcion > 4:
+    while opcion < 0 or opcion > 5:
         try:
             opcion = int(input())
         except:
             opcion = -1
-        if opcion < 0 or opcion > 4:
-            print("Elige una opción válida\n0. Salir\n1. Suma\n2. Resta\n3. Multiplicación\n4. División")
+        if opcion < 0 or opcion > 5:
+            print("Elige una opción válida\n0. Salir\n1. Suma\n2. Resta\n3. Multiplicación\n4. División\5. Libre")
     if opcion == 0:
         exit(0)
     if opcion == 1: 
@@ -19,53 +22,71 @@ def elegirOperacion():
         operacion = "multiplicación"
     if opcion == 4: 
         operacion = "división"
+    if opcion == 5: 
+        operacion = "libre"
     return opcion, operacion
 
 def suma(n1, n2):
-    print(f"Resultado: ", (int(n1) + int(n2)) )
+    print(f"Resultado: ", (float(n1) + float(n2)) )
 
 def resta(n1, n2):
-    print(f"Resultado: ", (int(n1) - int(n2)) )
+    print(f"Resultado: ", (float(n1) - float(n2)) )
 
 def mult(n1, n2):
-    print(f"Resultado: ", (int(n2) * int(n2)) )
+    print(f"Resultado: ", (float(n2) * float(n2)) )
 
 def div(n1, n2):
     num = 0
     error = False
     try:
-        num = (int(n1) / int(n2))
+        num = (float(n1) / float(n2))
     except:
         error = True
         print(f"Error Cálculo: No es posible dividir entre 0")
     if not error: print(f"Resultado: ", num )
 
+def libre():
+    print("En el modo libre puedes escribir los cuatro tipos de operaciones y darle al enter para obtener la respuesta.\nLos simbolos desponibles son '+', '-', '*', '/' y '%' \nPara salir escriba 'exit'.")
+    salir = False
+    while salir == False:
+        try:
+            respuesta = input()
+            if respuesta == "exit" or respuesta == "Exit" or respuesta == "salir" or respuesta == "Salir":
+                break
+            print(eval(respuesta))
+        except:
+            print("Error Sintaxis")
+    return
 
-opcion,operacion = elegirOperacion()
-print(f"Introduzca dos numeros para la operación de " + operacion + ".")
+#MAIN
+while True:
+    opcion,operacion = elegirOperacion()
+    if opcion!=5: 
+        print(f"Introduzca dos numeros para la operación de " + operacion + ".")
+        errorNum = False
+        try:
+            x = float(input())
+            y = float(input())
+        except:
+            errorNum = True
+        while errorNum == True:
+            errorNum = False
+            print("No se permiten caracteres que no sean números\nIntroduzca dos numeros para la " + operacion + ".")
+            try:
+                x = float(input())
+                y = float(input())
+            except:
+                errorNum = True
 
-errorNum = False
-try:
-    x = int(input())
-    y = int(input())
-except:
-    errorNum = True
-while errorNum == True:
-    errorNum = False
-    print("No se permiten caracteres que no sean números\nIntroduzca dos numeros para la " + operacion + ".")
-    try:
-        x = int(input())
-        y = int(input())
-    except:
-        errorNum = True
-
-if opcion == 1: 
-    suma(x, y)
-if opcion == 2: 
-    resta(x, y)
-if opcion == 3:
-    mult(x,y)
-if opcion == 4:
-    div(x,y)
+    if opcion == 1: 
+        suma(x, y)
+    if opcion == 2: 
+        resta(x, y)
+    if opcion == 3:
+        mult(x,y)
+    if opcion == 4:
+        div(x,y)
+    if opcion == 5:
+        libre()
 
 
