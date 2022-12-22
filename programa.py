663b1035244a136903aebda0c828fa3b7a2eac6f
Victor Hernandez Justamante <vhj1@gcloud.ua.es>
2022-12-22 17:03:38 +0100

    v1.4

--- programa.py
+++ programa.py
@@ -4,38 +4,35 @@ def suma(n1, n2):
 def resta(n1, n2):
     print(f"Resultado: ", (int(n1) - int(n2)) )
 
+def mult(n1, n2):
+    print(f"Resultado: ", (int(n2) * int(n2)) )
 
-print("¿Que operación quiere realizar?\n1. Suma\n2. Resta")
+
+print("¿Que operación quiere realizar?\n1. Suma\n2. Resta\n3. Multiplicación")
 operacion = ""
-opcion = ""
-while opcion!= "1" and opcion!="2":
-    opcion = input()
-    if opcion!= "1" and opcion!="2":
-        print("Elige una opción válida\n1. Suma\n2. Resta")
-if opcion == "1": 
-    operacion = "suma"
-else:
-     operacion = "resta"
-print(f"Introduzca dos numeros para la operación de " + operacion + ".")
-
-errorNum = False
-try:
-    x = int(input())
-    y = int(input())
-except:
-    errorNum = True
-while errorNum == True:
-    errorNum = False
-    print("No se permiten caracteres que no sean números\nIntroduzca dos numeros para la " + operacion + ".")
+opcion = 0
+while opcion < 1 or opcion > 3:
     try:
-        x = int(input())
-        y = int(input())
+        opcion = int(input())
     except:
-        errorNum = True
+        opcion = 0
+    if opcion < 1 or opcion > 3:
+        print("Elige una opción válida\n1. Suma\n2. Resta\n3. Multiplicación")
+if opcion == 1: 
+    operacion = "suma"
+if opcion == 2: 
+    operacion = "resta"
+if opcion == 3: 
+    operacion = "multiplicación"
+print(f"Introduzca dos numeros para la operación de " + operacion + ".")
+x = input()
+y = input()
 
-
-if opcion == "1": 
+if opcion == 1: 
     suma(x, y)
-if opcion == "2": 
+if opcion == 2: 
     resta(x, y)
+if opcion == 3:
+    mult(x,y)
+
 
